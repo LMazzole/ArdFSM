292163df7d6ead66040d8c1dd7bb99e7ce831d3b
Luca Mazzoleni <lmazzole@hsr.ch>
2019-06-10 11:53:50 +0200

    changed function order from alphabetical to declaration

--- docs/class_door.js
+++ docs/class_door.js
@@ -1,10 +1,10 @@
 var class_door =
 [
     [ "Door", "class_door.html#af80d9ba0686dd171ddfd3886f7ce317a", null ],
+    [ "open", "class_door.html#aa65d963980b9b3b2afaafcfd15534057", null ],
     [ "close", "class_door.html#a09a4060eee58af4206f9f1d863dc21d3", null ],
     [ "init", "class_door.html#a1537b628dfb5e085cc24c885dd6d58ca", null ],
-    [ "open", "class_door.html#aa65d963980b9b3b2afaafcfd15534057", null ],
-    [ "pActualPos", "class_door.html#acfcabd4a97e284684be6e37051e6f0a7", null ],
     [ "pPosClosed", "class_door.html#ad54ac583ea8dd5818ccf7a8c25b008a2", null ],
-    [ "pPosOpen", "class_door.html#a06a5daf75a6992a04b6b5b791d8a1091", null ]
+    [ "pPosOpen", "class_door.html#a06a5daf75a6992a04b6b5b791d8a1091", null ],
+    [ "pActualPos", "class_door.html#acfcabd4a97e284684be6e37051e6f0a7", null ]
 ];
